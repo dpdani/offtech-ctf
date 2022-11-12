eb5b16e5f3e4ec1928b3475e4a181fceaf3fcb67
Marcello Meschini <marcello.meschini@studenti.unitn.it>
2022-11-12 15:03:27 +0100

    Added apache installer script

---
+++ blue/server_installer.sh
@@ -0,0 +1,17 @@
+#!/usr/local/bin/bash
+
+EXPERIMENT="ctf-resilient-g3"
+PROJECT="offtech"
+
+ssh server.${EXPERIMENT}.${PROJECT} "sudo apt-get update; sudo apt-get install -y apache2"
+
+ssh server.${EXPERIMENT}.${PROJECT} "echo '1' | sudo tee /var/www/html/1.html;
+echo '2' | sudo tee /var/www/html/2.html;
+echo '3' | sudo tee /var/www/html/3.html;
+echo '4' | sudo tee /var/www/html/4.html;
+echo '5' | sudo tee /var/www/html/5.html;
+echo '6' | sudo tee /var/www/html/6.html;
+echo '7' | sudo tee /var/www/html/7.html;
+echo '8' | sudo tee /var/www/html/8.html;
+echo '9' | sudo tee /var/www/html/9.html;
+echo '10' | sudo tee /var/www/html/10.html;"
