3221aab87b6c58d368a52bb0748f23b7341530dc
alan-gassine <gassinealan@gmail.com>
2022-11-12 11:39:01 -0500

    modified legitimate (rate and GET 404)

--- red/legitimate.py
+++ red/legitimate.py
@@ -1,7 +1,21 @@
-import urllib.request
-import time, random
+import urllib.request, urllib.error
+import time, random, sys
 
-server_ip = "http://10.1.5.2"
+server_ip = "http://10.1.5.2/cmFuZG9tdXJs"
+
+if len(sys.argv) != 2:
+    print("Usage: python3 legitimate.py 1-9")
+    exit(0)
+rate = int(sys.argv[1])
+if rate > 9:
+    rate = 9
+elif rate < 1:
+    rate = 1
+
 while True:
-    contents = urllib.request.urlopen(server_ip).read()
-    time.sleep(random.randint(1,10))
+    try:
+        contents = urllib.request.urlopen(server_ip).read() # GET requests the server
+    except urllib.error.HTTPError as e:
+        pass # Handle 404 and ignores it because be actually want to request a 404
+    time.sleep(rate + random.uniform(-1, 1)) # Sleeps a random number of seconds between rate-1 and rate+1
+    
