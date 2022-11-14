7fd06500898b69d6ccfef91257abcf345366836c
Student-otech2ai OffTech <otech2ai@users.isi.deterlab.net>
2022-11-14 10:48:10 -0800

    Modified capture.sh to work also in the gateway

--- blue/capture.sh
+++ blue/capture.sh
@@ -3,11 +3,10 @@
 #set -x # echo on
 
 EXPERIMENT="ctf-resilient-g3"
-SERVER="server.${EXPERIMENT}.offtech"
-FILENAME="capture"
 DST_IP="10.1.2.2"
 
-ETH=$(ssh ${SERVER} "sudo ip route get ${DST_IP}" | head -1 | awk '{print $5}')
+ETH=$(ssh $1.${EXPERIMENT}.offtech "sudo ip route get ${DST_IP}" | head -1 | awk '{print $5}')
 
 rm -f "~/offtech-ctf/blue/pcap.pcap"
-ssh ${SERVER} "sudo tcpdump -s0 -U --immediate-mode -nn -i ${ETH} -w ~/offtech-ctf/blue/pcap.pcap"
+ssh $1.${EXPERIMENT}.offtech "sudo tcpdump -s0 -U --immediate-mode -nn -i ${ETH} -w ~/offtech-ctf/blue/pcap.pcap"
+
